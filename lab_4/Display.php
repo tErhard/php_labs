0dbfcb8ba28fa297237ca7982c2bd2e1b58dc654
telingerErhard <93592012+tErhard@users.noreply.github.com>
2022-11-09 23:37:22 +0200

    Update Display.php

--- lab_4/Display.php
+++ lab_4/Display.php
@@ -9,8 +9,15 @@ class Display
         $table .= '<tr> <th>id</th> <th>full_name</th> <th>sex</th> <th>born</th> <th>education</th> <th>speciality</th> <th>date_of_registration</th> </tr>';
 
         foreach ($array as $item) {
-            $table .= "<tr><td>$item->id</td><td>$item->full_name</td><td>$item->sex</td><td>$item->born</td>" .
-                "<td>$item->education</td><td>$item->speciality</td><td>$item->date_of_registration</td></tr>";
+            $table .=
+                "<tr><td>" . $item['id'] .
+                "</td><td>" . $item['full_name'] .
+                "</td><td>" . $item['sex'] .
+                "</td><td>" . $item['born'] .
+                "</td><td>" . $item['education'] .
+                "</td><td>" . $item['speciality'] .
+                "</td><td>" . $item['date_of_registration'] .
+                "</td></tr>";
         }
 
         $table .= '</table>';
