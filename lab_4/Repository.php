e22be27e9c2a625492ea115a3432ee9cd2b74cb3
telingerErhard <93592012+tErhard@users.noreply.github.com>
2022-11-09 23:38:21 +0200

    Update Repository.php

--- lab_4/Repository.php
+++ lab_4/Repository.php
@@ -2,15 +2,44 @@
 
 class Repository
 {
-    public static function savePeoples($array)
+    public $dbh;
+
+    public function __construct($dbh)
     {
-        $file = fopen("peoples.txt", "w");
-        fwrite($file, serialize($array));
-        fclose($file);
+        $this->dbh = $dbh;
     }
 
-    public static function loadPeoples()
+    public function createPeoples($array)
     {
-        return unserialize(file_get_contents("peoples.txt"));
+        $this->dbh->query('INSERT INTO Peoples(full_name, sex, born, education, date_of_registration) VALUES (' .
+            "'" . $array['full_name'] . "', " .
+            "'" . $array['sex'] . "', " .
+            "'" . $array['born'] . "', " .
+            "'" . $array['education'] . "', " .
+            "'" . $array['speciality'] . "', " .
+            "'" . $array['date_of_registration'] . "')"
+        );
+    }
+
+    public function readPeoples()
+    {
+        return $this->dbh->query('SELECT * FROM Peoples')->fetchAll();
+    }
+
+    public function updatePeople($array)
+    {
+        $this->dbh->query('UPDATE Clients SET ' .
+            'full_name = ' . $array['full_name'] . ', ' .
+            'sex = ' . $array['sex'] . ', ' .
+            'born = ' . $array['born'] . ', ' .
+            'education = ' . $array['education'] . ', ' .
+            'speciality = ' . $array['speciality'] . ' , ' .
+            'date_of_registration = ' . $array['date_of_registration'] . ' ' .
+            'WHERE id = ' . $array['id']);
+    }
+
+    public function deletePeople($array)
+    {
+        return $this->dbh->query('DELETE FROM Peoples WHERE id = ' . $array['id']);
     }
 }
